659e974e30cbed639e1a85eacae392b613e0090d
Neve700 <147994506+Neve700@users.noreply.github.com>
2024-08-07 19:44:36 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
-module.exports = function toReadable (number) {
-  
-}
+module.exports = function toReadable (n) {
+  if (n in numsToWords) {
+    return numsToWords[n];
+  };
+
+    const hundreds = Math.floor( n / 100);
+    const tens = Math.floor( n % 100 / 10) * 10;
+    const units = n % 10;
+
+
+    if (n >= 100 && n % 100 === 0) {
+        return `${numsToWords[hundreds]} hundred`;
+    }
+    else if (n >= 100 && n % 100 > 0) {
+        return `${numsToWords[hundreds]} hundred ${numsToWords[tens]}`;
+    }
+    else if (n > 100) {
+        return `${numsToWords[hundreds]} hundred ${numsToWords[tens]} ${numsToWords[units]}`; 
+    };
+
+    if (n < 100 && n >= 20) {
+        return `${numsToWords[tens]} ${numsToWords[units]}`;
+    };
+        
+};
